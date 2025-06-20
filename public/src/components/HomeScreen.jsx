36b5d813275a6a0f2f644ad546cc612584f9c3dd
rashidultg <rashidulislamtg@gmail.com>
2025-06-21 01:18:11 +0600

    Create HomeScreen.jsx

---
+++ public/src/components/HomeScreen.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomeScreen() {
+  return (
+    <div className="p-4 text-center">
+      <h1 className="text-xl font-bold">🎮 TON GameBox</h1>
+      <p>Welcome to the TON Mini App</p>
+    </div>
+  );
+}
